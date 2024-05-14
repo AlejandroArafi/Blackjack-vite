1bcede5c07a21ccd393dcd4bcf8e769787aff5de
Alejandro <alejandro.arafi@gmail.com>
2024-05-14 10:39:37 -0400

    primer commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * @param {Array<String>} deck
+ * @returns {String}
+ */
+
+export const pedirCarta = (deck) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw new Error ('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
+
+ 
